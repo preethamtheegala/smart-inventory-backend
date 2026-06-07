deea2e2376055f42002ebd8de9dbbea6913e31ae
Preetham Theegala <preethamtheegala@gmail.com>
2026-06-07 13:47:25 +0530

    Add user creation endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from .views import ProductViewSet, SaleViewSet, login_view
+from .views import create_default_users
+
 
 router = DefaultRouter()
 
@@ -9,6 +11,7 @@ router.register(r'sales', SaleViewSet)
 
 urlpatterns = [
     path('login/', login_view),
+    path('create-users/', create_default_users),
 ]
 
 urlpatterns += router.urls
